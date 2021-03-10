d9eccc29cdfd72b106678e52b60ca394bb92472e
samuelbojela <bojela@gmail.com>
2021-03-09 21:02:12 -0500

    Working local commit

--- local/coursecatalogue/index.php
+++ local/coursecatalogue/index.php
@@ -32,73 +32,28 @@ require_login();
 
 echo $OUTPUT->header();
 
-
-//echo '<hr><p>test <span style = "margin:50px">test 2</span></p></hr>';
-/**
- * This is just for testing
- * $sql = "SELECT u.firstname,u.lastname from mdl_user as u where id in (select id from mdl_role_assignments where roleid in( select id from mdl_role where shortname = 'editingteacher'))";
- * //echo '<p> Query: <b>' . $sql . '</b></p>';
- * $results = $DB->get_records_sql($sql);
- * echo 'Query results:';
- * 
- * Testing purposes
- * print_r ($results);
-*/
-/** 
- * $userid = $DB->get_record_select('user', 'username=? ',array('admin'));echo $userid->firstname;
- * echo ' ';
- * echo $userid->lastname;
- * echo '</br>';
- * $role = $DB->get_record_select('role_assignments', 'userid =?', array($userid->id));
- * echo $role->id;
- * echo '</br>';
- * $roleassignments = $DB->get_record_select('role', 'id =?', array($role->id));
- * echo $roleassignments->shortname;
-*/
-/**
-*$getrole = $DB->get_record_select('role', 'shortname=?', array('editingteacher'));
-
-*Display the teacher role id >>Just incase purposes
-*echo $getrole->id;
-*echo '<br>';
-
-*$getteachers = $DB->get_records_select('role_assignments','roleid=?', *array($getrole->id));
-*print_r($getteachers);
-*echo '<br>';
-*
-*foreach($getteachers as $showteachers){
-*  $getteacherinfo = $DB->get_record_select('user', 'id=?', array
-* ($showteachers->userid));
-*
-*  echo 'Teacher Name: ';
-*  echo $getteacherinfo->firstname;
-*  echo ' ';
-*  echo $getteacherinfo->lastname;
-*  echo '<br>';
-*}
-*/
 echo $OUTPUT->render_from_template('local_coursecatalogue/searchbar', []);
 echo '<div class="card">';
 echo '<table class="table table-striped">';
 echo '<thead>';
 echo '<tr>';
 echo '<th>Course</th>';
-echo '<th></th>';
 echo '<th>Start Date</th>';
 echo '<th>Instructor</th>';
 echo '<th>Tags</th>';
 echo '<th>Ratings</th>';
 echo '</tr>';
+echo '</thead>';
 
-$sql = "SELECT * FROM {course} WHERE ID is not null and fullname <> 'Local Environment'";
+$sql = "SELECT * FROM {course} WHERE ID is not null and ID <> 1";
 $courses = $DB->get_records_sql($sql);
   sort($courses);
 
 foreach($courses as $course){
-  echo '<br>';
+  
   echo '<tr>'; 
-  echo '<td>'.'<a href=/lms-moodle/course/view.php?id='.$course->id.'>'.$course->fullname.'</a>'.'<td>';
-  echo '<td>'.date('M-d-Y hA', $course->startdate).'<td>';
+  echo '<td>'.'<a href=/lms-moodle/course/view.php?id='.$course->id.'>'.$course->fullname.'</a>'.'</td>';
+  echo '<td>'.date('M-d-Y hA', $course->startdate).'</td>';
   //next line
   
 
@@ -108,38 +63,36 @@ JOIN {role} as r ON ra.roleid = r.id
 JOIN {context} as con ON ra.contextid = con.id
 JOIN {course} as c ON c.id = con.instanceid AND con.contextlevel = 50
 WHERE r.shortname = 'editingteacher'";
-
+   echo '<td>';
 $teachers = $DB->get_records_sql($sql);
  foreach($teachers as $teacher){
-
    echo $teacher->firstname;
    echo ' ';
    echo $teacher->lastname;   
    echo ' ';   
-   
  }
-   echo '<td>';
+ echo '</td>';
  
 $sql = "SELECT t.name FROM mdl_tag AS t 
 JOIN mdl_tag_instance AS ti ON ti.tagid = t.id
 JOIN mdl_context AS ctx ON ctx.`contextlevel` = 50";
 
+
 $tags = $DB->get_records_sql($sql);
+echo '<td>';
 foreach($tags as $tag){
   echo $tag->name;
 }
-echo '<td>';
+echo '</td>';
 
+echo '<td>';
 $block = block_instance('rate_course');
 $block->display_rating($course->id);
 echo '</td>';
 };
-
 echo '</tr>';
 
+echo '</table>';
+
 echo $OUTPUT->footer();
-
-
-
-//print_r($courses);
 
