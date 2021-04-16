6431a4a66f6f098c8546985f243c56f86711a24b
root <root@rc-lmsqa.partners.org>
2021-04-16 09:52:27 -0400

    je483 :: reverting the config.php that was mucked up in the merges

--- config.php
+++ config.php
@@ -11,24 +11,24 @@ $CFG = new stdClass();
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'mysql4.research.partners.org:3306';
-$CFG->dbname    = 'lmsprod';
+$CFG->dbname    = 'lmsqa';
 $CFG->dbuser    = 'headmaster';
-$CFG->dbpass    = '1qazxsw2!QAZXSW@';
+$CFG->dbpass    = 'g@Me_P*cK+!bC=617';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
   'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_general_ci',
+  'dbcollation' => 'utf8_general_ci',
 );
 
-$CFG->wwwroot   = 'https://opencourses.partners.org';
-$CFG->dataroot  = '/var/www/moodledata';
+$CFG->wwwroot   = 'https://rc-lmsqa.partners.org';
+$CFG->dataroot  = '/var/www/moodledata/';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
+
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
-
