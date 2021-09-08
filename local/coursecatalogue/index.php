9e3a6a31f834da69cafc50edd20da2660a2c859d
Oyler Castrillo, Miguel <moylercastrillo@mgh.harvard.edu>
2021-09-08 13:43:46 +0000

    changed "department of.." to "division" of clinical research for department dropdown menu option pre-select

--- local/coursecatalogue/index.php
+++ local/coursecatalogue/index.php
@@ -122,7 +122,7 @@ if (isset($_GET['department'])){
   $dept = $_GET['department'];
   switch($dept){ //so far we have 3 departments 1 -> DCR, 2 -> CFD, 3-> MGRI, check mdl_customfield_field options column for any new options
     case "1":
-      $context = array_push_assoc($context, 'departmentname', "Department of Clinical Research");
+      $context = array_push_assoc($context, 'departmentname', "Division of Clinical Research");
       break;
     case "2":
       $context = array_push_assoc($context, 'departmentname', "Center for Faculty Development");
