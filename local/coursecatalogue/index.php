362d3505a63b246c95252961deca20ac1432a692
Samuel <sbojela@mgh.harvard.edu>
2021-10-04 10:02:49 -0400

    Added some logic to bring in the second assigned teacher.

--- local/coursecatalogue/index.php
+++ local/coursecatalogue/index.php
@@ -689,8 +689,8 @@ if ($on_demand_flag == 0){
 
     $sql = "SELECT u.firstname, u.lastname
             FROM {user} u, {role_assignments} r_a, {role} r, {enrol} e, {user_enrolments} u_e
-            WHERE e.courseid = ". $course->id ." AND u.id = r_a.userid AND (r_a.roleid = 4 OR r_a.roleid = 3) AND u_e.userid = u.id AND e.id = u_e.enrolid AND 
-            order by u_e.id asc"; //Swapped u.firstname <> 'DCR' for order by u_e.id asc because the order dictates the user that gets selected. 
+            WHERE e.courseid = ". $course->id ." AND u.id = r_a.userid AND (r_a.roleid = 4 OR r_a.roleid = 3) AND u_e.userid = u.id AND e.id = u_e.enrolid 
+            AND u.firstname <> 'DCR' order by u_e.id asc"; //Added order by u_e.id asc because the order dictates the user that gets selected. 
             
     echo '<td>';
     $teachers = $DB->get_records_sql($sql); 
