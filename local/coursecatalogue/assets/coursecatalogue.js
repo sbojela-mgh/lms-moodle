9b48d4f0c8a77b6630a871895260bb507a794600
La <TLA@mgh.harvard.edu>
2022-03-06 23:58:18 -0500

    testing

--- local/coursecatalogue/assets/coursecatalogue.js
+++ local/coursecatalogue/assets/coursecatalogue.js
@@ -1,35 +1,28 @@
 //These requires are set for our use
-require(['core/first', 'jquery','jqueryui', 'core/ajax'], function(core, $, bootstrap, ajax) {
-   
-    $(document).ready(function(){
-       
-        $('#search').click(function(){
-            searchusers();
-        });
+require(['core/first', 'jquery', 'jqueryui', 'core/ajax'], function(core, $, bootstrap, ajax) {
 
-<<<<<<< HEAD
-        function searchusers() {
-            console.log('search users');
-            window.open("/local/coursecatalogue/index.php?month=" + $('#month').val() + "&year=" + $('#year').val(), '_self');
-        }
-=======
-  //Using Jquery, when the page loads, this function will load
-  $(document).ready(function() {
-  
-    //Params finds all of the key values and append it to  
-    var params = {};
-      window.location.search
-      .replace(/[?&]+([^&]+)=([^&]*)/gi, function(str, key, value) {
-        params[key] = value;
-      });
-      if (params['month']){
-        $('#month option[value=' + params['month']+ ']').attr('selected', 'selected');
-      }
-      if (params['year']){
-        $('#year option[value=' + params['year']+ ']').attr('selected', 'selected');
-      }
-    $('#search').click(function(){
-      searchcourses();
->>>>>>> origin/lms-test
-    });
-});
+            $(document).ready(function() {
+
+                        $('#search').click(function() {
+                            searchusers();
+                        });
+
+                        //Using Jquery, when the page loads, this function will load
+                        $(document).ready(function() {
+
+                            //Params finds all of the key values and append it to  
+                            var params = {};
+                            window.location.search
+                                .replace(/[?&]+([^&]+)=([^&]*)/gi, function(str, key, value) {
+                                    params[key] = value;
+                                });
+                            if (params['month']) {
+                                $('#month option[value=' + params['month'] + ']').attr('selected', 'selected');
+                            }
+                            if (params['year']) {
+                                $('#year option[value=' + params['year'] + ']').attr('selected', 'selected');
+                            }
+                            $('#search').click(function() {
+                                searchcourses();
+                            });
+                        });
